176ccde44bd93c676e8e2e02ad5786613bf31511
ariel0080 <ariel0080@gmail.com>
2018-07-03 09:28:47 -0300

    2_PARCIAL- MODIFICACIONES EN CALCULO DE SUELDO

---
+++ PARCIAL_2_BRINO/2PARCIAL/eEmpleado.h
@@ -0,0 +1,49 @@
+#ifndef __EMPLOYEE
+#define __EMPLOYEE
+struct
+{
+    int id;
+    char nombre[51];
+    char direccion[51];
+    int hora;
+    int sueldo;
+
+}typedef eEmpleado;
+#endif // __EMPLOYEE
+
+//FUNCION COMPARA UTILIZADA DENTRO DEL SORT
+
+int compareeEmpleado(void* pEmployeeA,void* pEmployeeB);
+
+//FUNCIONES PARA IMPRIMIR DATOS
+
+void printeEmpleado_sueldo(eEmpleado* p);
+void m_empleado_sueldo(eEmpleado*);
+void m_empleado(eEmpleado* lEmp);
+void printeEmpleado(eEmpleado* p);
+
+//CONTRUCTOR
+
+eEmpleado* neweEmpleado();
+
+//SETTERS 1 OK 0 NO SETEO
+
+int set_Empleado_id(eEmpleado* p,int id);
+int set_Empleado_nombre(eEmpleado* p,char* n);
+int set_Empleado_domicilio(eEmpleado* p,char* d);
+int set_Empleado_hora(eEmpleado* p,int h);
+int set_Empleado_sueldo(eEmpleado* p,int s);
+
+//GETTERS
+
+int get_Empleado_id(eEmpleado*);
+int get_Empleado_hora(eEmpleado*);
+int get_Empleado_sueldo(eEmpleado*);
+void get_Empleado_nombre(eEmpleado* ,char* );
+void get_Empleado_direccion(eEmpleado*,char* );
+
+//CALCULA SUELDO PARA FUNCION MAP
+
+int calc_suel_Empleado(void* empleado);
+
+
---
+++ PARCIAL_2_BRINO/2PARCIAL/funciones.h
@@ -0,0 +1,1 @@
+void guardarArchivo(ArrayList* empleados);
